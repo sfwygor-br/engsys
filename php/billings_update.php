38820240bbfe6334632f70298b6d27b7646f22fe
sfwygor-br <felipe+github@masteryidea.com>
2018-12-07 16:21:27 -0200

    Branch 6.1
    Atualização Baixa de Contas

--- php/billings_update.php
+++ php/billings_update.php
@@ -7,6 +7,18 @@
 	
 	$action = @$_POST["payment_date"];
 	
+	$response = "";
+	if (@$_GET['action'] == 'CANCEL'){
+		$sql = "update billing set payment_date = null, value_payed = null where 1 = 1 ".$_GET["sql_macro"];
+		echo $sql;
+		$rs  = mysqli_query($GLOBALS["conn"], $sql);
+		if ($rs == True){
+			$response = "<script>alert('Pagamento cancelado.');<script>";
+		}else{
+			$response = "<script>alert('O pagamento não pode ser cancelado -: ".mysqli_error($rs)."');<script>";
+		}
+	}
+	
 	connect();
 	$page = 'billing';
 	
@@ -30,12 +42,18 @@
 		
 	$sql = "select b.*,
                    (select description from event e where e.idevent = b.idevent) as event,
-                   concat(' and idbilling = ', idbilling) as sql_macro				   
+                   concat(' and idbilling = ', idbilling) as sql_macro,
+				   case when b.value_payed is null then
+				       'Baixar'
+				   else
+						'Cancelar'
+				   end btn_title
 	          from billing b
 			 where 1 = 1
 			   $action
 			   and iduser_integ = ".$_SESSION["iduser_integ"];
 	$rs = mysqli_query($GLOBALS["conn"], $sql);
+	$i = 1;
 	while ($r = mysqli_fetch_assoc($rs)){
 		$class = "";
 		if ($i % 2){
@@ -49,11 +67,19 @@
 				<td>".$r["idbilling"]."</td>
 				<td>".$r["event"]."</td>
 				<td>".$r["value"]."</td>
-				<td><a href='./billings.php?action=ALTERSTATUS&sql_macro=".$r["sql_macro"]."'>Baixar</a></td>
+				";
+			if ($r["btn_title"] == 'Baixar'){
+				$section = $section . "<td><a href='./billings.php?action=ALTERSTATUS&sql_macro=".$r["sql_macro"]."'>".$r["btn_title"]."</a></td>
 			</tr>
-		";
+			";
+			}else{
+				$section = $section . "<td><a href='./billings_update.php?action=CANCEL&sql_macro=".$r["sql_macro"]."'>".$r["btn_title"]."</a></td>
+			</tr>
+			";
+			}
 	}
-	$section = $section . "</table>";
+	$section = $section . "</table> 
+	                       $response";
 	
 	ob_end_clean();		
 	load_base_page("Baixa/Cancelamento de Baixas", $page, $section);
